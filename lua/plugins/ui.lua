694100b40f651f955da70bc5060c160224042af3
zyth3519 <zythpang@gmail.com>
2026-01-16 10:25:46 +0800

    modify which-key

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -37,21 +37,14 @@ return {
         end,
     },
     {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
-}
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            ---@type false | "classic" | "modern" | "helix"
+            preset = "helix",
+            win = {
+                border = "none",
+            }
+        },
+    }
 }
