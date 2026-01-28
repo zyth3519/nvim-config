6e6109ba39311641125205ecc048d9d730aaf775
zyth3519 <zythpang@gmail.com>
2026-01-28 08:56:53 +0800

    修改文件结构

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -92,7 +92,5 @@ return {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         config = true
-        -- use opts = {} for passing setup options
-        -- this is equivalent to setup({}) function
     }
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -51,8 +51,21 @@ return {
     {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
         opts = {},
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        opts = {
+            -- 核心配置
+            size = 10,        -- 水平分割终端的默认高度（垂直分割时为宽度）
+            direction = 'horizontal', -- 终端默认打开方式：horizontal（水平）/ vertical（垂直）/ float（悬浮）
+            close_on_exit = true, -- 终端进程退出后自动关闭终端窗口
+            shade_terminals = true, -- 终端窗口阴影效果（提升美观度）
+            float_opts = {
+                border = 'rounded', -- 悬浮终端的圆角边框
+                winblend = 10, -- 悬浮终端透明度
+            },
+        },
     }
 }
