230a96e53d65e43af7595b7ba9a72ff609770da8
zyth3519 <zythpang@gmail.com>
2026-03-01 19:48:34 +0800

    删除无用代码

--- lua/config/cmd.lua
+++ lua/config/cmd.lua
@@ -8,24 +8,6 @@ vim.api.nvim_create_user_command("Run", function(opts)
 	end
 end, { nargs = "*", desc = "带参数打开底部终端" })
 
-local function scandir(path)
-	local entries = {}
-	local handle = vim.loop.fs_scandir(path)
-	if not handle then
-		return entries
-	end
-	while true do
-		local name, type = vim.loop.fs_scandir_next(handle)
-		--   name = 文件名
-		--   type = "file" | "directory" | "link" | ...
-		if not name then
-			break
-		end
-		table.insert(entries, { name = name, type = type })
-	end
-	return entries
-end
-
 vim.api.nvim_create_user_command("Session", function(opts)
 	if #opts.fargs == 0 then
 		return
