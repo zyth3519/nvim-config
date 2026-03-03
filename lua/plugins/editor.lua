6d309f9bd1c3eb779c809cb121a5e3ce8cee640a
zyth3519 <zythpang@gmail.com>
2026-03-03 21:20:45 +0800

    feat: add folke/flash.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -174,4 +174,37 @@ return {
 			})
 		end,
 	},
+
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
+	},
 }
