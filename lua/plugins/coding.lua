bd7e460f55a7fa56691e00cda569eab4b83cf95b
zyth3519 <zythpang@gmail.com>
2026-03-10 10:52:34 +0800

    配置优化

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -107,4 +107,19 @@ return {
 			}
 		end,
 	},
+
+	-- 优秀的折叠插件 (Ufo)
+	{
+		"kevinhwang91/nvim-ufo",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "kevinhwang91/promise-async" },
+		config = function()
+			-- treesitter
+			require("ufo").setup({
+				provider_selector = function()
+					return { "treesitter", "indent" }
+				end,
+			})
+		end,
+	},
 }
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,19 +1,4 @@
 return {
-	-- 优秀的折叠插件 (Ufo)
-	{
-		"kevinhwang91/nvim-ufo",
-		event = { "BufReadPost", "BufNewFile" },
-		dependencies = { "kevinhwang91/promise-async" },
-		config = function()
-			-- treesitter
-			require("ufo").setup({
-				provider_selector = function()
-					return { "treesitter", "indent" }
-				end,
-			})
-		end,
-	},
-
 	-- 多光标支持 (Multicursor.nvim)
 	{
 		"jake-stewart/multicursor.nvim",
@@ -90,36 +75,10 @@ return {
 			})
 		end,
 	},
-	{
-		"stevearc/overseer.nvim",
-		opts = {
-			component_aliases = {
-				-- 重新定义 "default" 别名
-				default = {
-					{ "on_exit_set_status" },
-					-- 加入你的自定义组件
-					{ "auto_show_result" },
-				},
-			},
-		},
-		config = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = { "OverseerList", "OverseerForm", "OverseerConfirm" },
-				callback = function(event)
-					local opts = { buffer = event.buf, noremap = true, silent = true }
-					vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
-					vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
-					vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
-					vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
-				end,
-			})
-		end,
-	},
 
 	{
 		"folke/flash.nvim",
 		event = "VeryLazy",
-		---@type Flash.Config
 		opts = {},
 		keys = {
 			{
