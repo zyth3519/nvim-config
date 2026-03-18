694dfc549b096242525ca88752ae1b5d9699611b
zyth3519 <zythpang@gmail.com>
2026-03-18 22:15:47 +0800

    补全按键描述

--- lua/config/autocmds/core.lua
+++ lua/config/autocmds/core.lua
@@ -3,13 +3,13 @@ vim.api.nvim_create_autocmd("TermOpen", {
 	pattern = "*",
 	callback = function()
 		local opts = { buffer = 0 }
-		vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-		vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-		vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-		vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-		vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-		vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-		vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+		vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], vim.tbl_extend("force", opts, { desc = "终端退出插入模式" }))
+		vim.keymap.set("t", "jk", [[<C-\><C-n>]], vim.tbl_extend("force", opts, { desc = "终端退出插入模式" }))
+		vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], vim.tbl_extend("force", opts, { desc = "终端跳转到左侧窗口" }))
+		vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], vim.tbl_extend("force", opts, { desc = "终端跳转到下方窗口" }))
+		vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], vim.tbl_extend("force", opts, { desc = "终端跳转到上方窗口" }))
+		vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], vim.tbl_extend("force", opts, { desc = "终端跳转到右侧窗口" }))
+		vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], vim.tbl_extend("force", opts, { desc = "终端窗口命令前缀" }))
 
 		vim.opt_local.number = false
 		vim.opt_local.relativenumber = false
