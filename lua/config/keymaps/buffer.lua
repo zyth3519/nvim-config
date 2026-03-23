5886053c67f9a5325120b4350f6407e545060268
zyth3519 <zythpang@gmail.com>
2026-03-23 19:48:50 +0800

    优化欢迎页缓冲区显示

--- lua/config/keymaps/buffer.lua
+++ lua/config/keymaps/buffer.lua
@@ -12,12 +12,74 @@ local function list_normal_buffers()
 	return buffers
 end
 
+local function open_welcome_buffer()
+	vim.cmd("enew")
+
+	local bufnr = vim.api.nvim_get_current_buf()
+	local win = vim.api.nvim_get_current_win()
+	local width = vim.api.nvim_win_get_width(win)
+	local height = vim.api.nvim_win_get_height(win)
+
+	local function center(text)
+		local pad = math.max(0, math.floor((width - vim.fn.strdisplaywidth(text)) / 2))
+		return string.rep(" ", pad) .. text
+	end
+
+	local body = {
+		center("Neovim"),
+		center("-------"),
+		"",
+		center("Quick Start"),
+		"",
+		center("<leader>ff  查找文件"),
+		center("<leader>e   打开 Oil"),
+		center("<M-x>       执行 Run"),
+		center(":Session    会话管理"),
+		"",
+		center("Press q to close"),
+	}
+
+	local top_padding = math.max(0, math.floor((height - #body) / 2)) - 10
+	local lines = {}
+
+	for _ = 1, top_padding do
+		table.insert(lines, "")
+	end
+
+	vim.list_extend(lines, body)
+
+	vim.bo[bufnr].buftype = "nofile"
+	vim.bo[bufnr].bufhidden = "wipe"
+	vim.bo[bufnr].buflisted = false
+	vim.bo[bufnr].swapfile = false
+	vim.bo[bufnr].modifiable = true
+
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+
+	vim.bo[bufnr].modifiable = false
+	vim.bo[bufnr].filetype = "starter"
+
+	vim.wo[win].number = false
+	vim.wo[win].relativenumber = false
+	vim.wo[win].signcolumn = "no"
+	vim.wo[win].cursorline = false
+	vim.wo[win].foldcolumn = "0"
+	vim.wo[win].colorcolumn = ""
+	vim.wo[win].spell = false
+
+	vim.keymap.set("n", "q", "<cmd>bd<cr>", {
+		buffer = bufnr,
+		silent = true,
+		desc = "关闭欢迎页",
+	})
+end
+
 local function close_current_buffer()
 	local current = vim.api.nvim_get_current_buf()
 	local buffers = list_normal_buffers()
 
 	if #buffers <= 1 then
-		vim.cmd("enew")
+		open_welcome_buffer()
 	end
 
 	if vim.api.nvim_buf_is_valid(current) and vim.bo[current].buflisted then
