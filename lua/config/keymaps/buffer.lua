37b051635befb2f3639b092d41dbc812475b8d63
zyth3519 <zythpang@gmail.com>
2026-03-23 19:34:37 +0800

    修复最后缓冲区关闭行为

--- lua/config/keymaps/buffer.lua
+++ lua/config/keymaps/buffer.lua
@@ -1,6 +1,32 @@
 -- 【缓冲区管理 (Buffer)】
-vim.keymap.set("n", "<C-q>", "<cmd>bd<cr>", { desc = "关闭当前文件(Buffer)" })
-vim.keymap.set("n", "<leader>q", "<cmd>bd<cr>", { desc = "关闭当前文件(Buffer)" })
+local function list_normal_buffers()
+	local buffers = {}
+	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].buflisted then
+			local bt = vim.bo[bufnr].buftype
+			if bt == "" then
+				table.insert(buffers, bufnr)
+			end
+		end
+	end
+	return buffers
+end
+
+local function close_current_buffer()
+	local current = vim.api.nvim_get_current_buf()
+	local buffers = list_normal_buffers()
+
+	if #buffers <= 1 then
+		vim.cmd("enew")
+	end
+
+	if vim.api.nvim_buf_is_valid(current) and vim.bo[current].buflisted then
+		vim.cmd("bd " .. current)
+	end
+end
+
+vim.keymap.set("n", "<C-q>", close_current_buffer, { desc = "关闭当前文件(Buffer)" })
+vim.keymap.set("n", "<leader>q", close_current_buffer, { desc = "关闭当前文件(Buffer)" })
 vim.keymap.set("n", "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", { desc = "只保留当前编辑的文件" })
 vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseLeft<cr>", { desc = "关闭左边所有缓冲区" })
 vim.keymap.set("n", "<leader>br", "<cmd>BufferLineCloseRight<cr>", { desc = "关闭右边所有缓冲区" })
