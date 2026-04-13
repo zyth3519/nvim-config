9778d9dcf6aacbb130ec0bd035ccfc3778726cad
zyth3519 <zythpang@gmail.com>
2026-04-13 23:14:44 +0800

    优化项目结构

--- lua/config/keymaps/init.lua
+++ lua/config/keymaps/init.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("config.keymaps.core")
-require("config.keymaps.groups")
 require("config.keymaps.buffer")
 require("config.keymaps.window")
 require("config.keymaps.search")
@@ -11,3 +10,22 @@ require("config.keymaps.git")
 require("config.keymaps.cursor")
 require("config.keymaps.coding")
 require("config.keymaps.debug")
+
+local wk_ok, wk = pcall(require, "which-key")
+if not wk_ok then
+	return
+end
+
+wk.add({
+	{ "<leader>b", group = "缓冲区 (Buffer)", icon = "󰓩" },
+	{ "<leader>c", group = "代码 (Code)", icon = "󰅩" },
+	{ "<leader>d", group = "调试 (Debug)", icon = "󰃤" },
+	{ "<leader>f", group = "文件 (File)", icon = "󰉋" },
+	{ "<leader>g", group = "版本控制 (Git)", icon = "󰊢" },
+	{ "<leader>s", group = "搜索 (Search)", icon = "󰍉" },
+	{ "<leader>w", group = "窗口 (Window)", icon = "󱂬" },
+	{ "g", group = "导航/跳转 (Go)", icon = "󰜎" },
+	{ "<leader>e", desc = "打开Oil", icon = "󰉋" },
+	{ "<leader>E", desc = "打开Oil（Root）", icon = "󰉋" },
+	{ "<leader>r", group = "运行 (Run)", icon = "󰆍" },
+})
