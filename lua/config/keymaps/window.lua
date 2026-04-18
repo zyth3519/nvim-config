b0d432101ec6f333ee2fd163664d5428ced4e312
zyth3519 <zythpang@gmail.com>
2026-04-18 18:22:52 +0800

    修改关闭窗口和buffer的逻辑

--- lua/config/keymaps/window.lua
+++ lua/config/keymaps/window.lua
@@ -5,4 +5,5 @@ vim.keymap.set("n", "<leader>wh", "<cmd>split<cr>", { desc = "水平分割当前
 vim.keymap.set("n", "<leader>wv", "<cmd>vsplit<cr>", { desc = "垂直分割当前窗口" })
 vim.keymap.set("n", "<leader>wx", "<C-w>x", { desc = "窗口互换" })
 vim.keymap.set("n", "<leader>wq", "<C-w>q", { desc = "关闭当前窗口" })
+vim.keymap.set("n", "<M-q>", "<C-w>q", { desc = "关闭当前窗口" })
 vim.keymap.set("n", "<leader>wo", "<cmd>only<cr>", { desc = "关闭其他所有窗口" })
